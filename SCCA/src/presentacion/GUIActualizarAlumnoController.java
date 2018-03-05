a3688939e00c54bcc958d67002026226bc0d22a4
Cristhian Ubaldo <Cris_UP@outlook.com>
2018-03-05 11:24:04 -0600

    Creación gui actualizar alumnos

--- SCCA/src/presentacion/GUIActualizarAlumnoController.java
+++ SCCA/src/presentacion/GUIActualizarAlumnoController.java
@@ -58,6 +58,7 @@ public class GUIActualizarAlumnoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
     
 }
